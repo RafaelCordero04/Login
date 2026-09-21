040c3f9d6552fb6fabf122b07862744c02bacb26
Rafael Cordero <corderodrafa@gmail.com>
2026-09-21 18:37:29 -0500

    Main del proyecto.

---
+++ src/main/java/com/example/login/Main.java
@@ -0,0 +1,27 @@
+package com.example.login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        // Carga la escena con las dimensiones aproximadas de la ventana de Scene Builder
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+
+        stage.setTitle("Conjunto Residencial - Login");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
